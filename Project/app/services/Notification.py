b4a995fa941929eefe4d42bd5c111b5dd2429109
weixiangtoh <weixiangtoh982@gmail.com>
2021-03-23 15:34:10 +0800

    Add Notifications microservice

---
+++ Project/app/services/Notification.py
@@ -0,0 +1,25 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+# import email html file
+with open('Email_content.html', 'r') as f:
+    html_string = f.read()
+
+# email address, subject and body
+message = Mail(
+    from_email='hotelenterprise@esd.sg',
+    to_emails='verwin.kwa.2019@smu.edu.sg',
+    subject='Testing sendgrid email services',
+    html_content=html_string)
+
+# sending email and printing status
+try:
+    sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sendgrid_client.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+except Exception as e:
+    print(e)
