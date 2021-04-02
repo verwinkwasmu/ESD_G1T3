ed10ed4db1a7522215dfcdb792586c9d9553f87d
weixiangtoh <weixiangtoh982@gmail.com>
2021-04-02 22:02:55 +0800

    Add working notification.py with AMPQ

--- Project/app/services/Order_Room_Service.py
+++ Project/app/services/Order_Room_Service.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import os
+import sys
 
 import requests
 from invokes import invoke_http
+
+import amqp_setup
+import pika
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -20,11 +25,13 @@ def order_room_service():
     if request.is_json:
         try:
             booking_details = request.get_json()
-            print("\nReceived a request to order room service in JSON:", booking_details)
+            print("\nReceived a request to order room service in JSON:",
+                  booking_details)
 
             # do the actual work
             # 1. Get all cart purchases using booking_id
-            result = processOrderRS(request.json.get('booking_id'), request.json.get('room_service_orders'))
+            result = processOrderRS(request.json.get(
+                'booking_id'), request.json.get('room_service_orders'))
             return jsonify(result), 200
 
         except Exception as e:
@@ -47,8 +54,8 @@ def processOrderRS(booking_id, room_service_orders):
     print('\n-----Invoking booking microservice-----')
     booking_result = invoke_http(booking_URL + "/" + booking_id)
     guest_details = {
-            "email": booking_result['data']['email'],
-            "guest_name": booking_result['data']['guest_name']
+        "email": booking_result['data']['email'],
+        "guest_name": booking_result['data']['guest_name']
     }
     print('booking_result:', guest_details)
 
@@ -68,34 +75,50 @@ def processOrderRS(booking_id, room_service_orders):
             "price": order['item_price']
         }
 
-        add_rs_result = invoke_http(cart_URL + "/add_rs/" + booking_id, method='POST', json=rs_order)
+        print('\n-----Invoking order microservice-----')
+        add_rs_result = invoke_http(
+            cart_URL + "/add_rs/" + booking_id, method='POST', json=rs_order)
         if add_rs_result['code'] not in range(200, 300):
 
             return {
                 'code': 404,
                 'status': 'failed to add room service'
             }
-    
-    # # 4. get all facilities & roomservices 
-    # all_facilities = invoke_http(facility_URL)
-    # all_room_services = invoke_http(room_service_URL)
-    
-    # # 5. Put into dictionary for item_names
-    # facility_names = {}
-    # for facility in all_facilities['data']['facilities']:
-    #     facility_names[facility['item_id']] = facility['item_name']
 
-    # room_service_names = {}
-    # for room_service in all_room_services['data']['room_services']:
-    #     room_service_names[room_service['item_id']] = room_service['item_name']
+        # Invoke the order microservice
+        print('add_rs_result:', add_rs_result)
 
-    # # 6. return all cart purchases with item_name
-    # for booking in cart_result['data']['bookings']:
-    #     if 'rs' in booking['item_id']:
-    #         booking['item_name'] = room_service_names[booking['item_id']]
-    #     else:
-    #         booking['item_name'] = facility_names[booking['item_id']]
-    
+        # Check the order result; if a failure, send it to the error microservice.
+        # code = add_rs_result["code"]
+        email_details = {
+            "email": booking_result['data']['email'],
+            "guest_name": booking_result['data']['guest_name'],
+            "quantity": order['rs_quantity'],
+            "price": order['item_price'],
+            "item_name" : order["item_name"],
+            "date_time": add_rs_result["data"]["order_datetime"],
+            "order_id": add_rs_result["data"]["order_id"],
+            "booking_id": add_rs_result["data"]["booking_id"]
+        }
+
+        print('add_rs_result:', email_details)
+        message = json.dumps(email_details)
+
+        amqp_setup.check_setup()
+
+        print('\n\n-----Publishing the (order notification) message with routing_key=order.notification-----')
+
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.notification",
+                                         body=message, properties=pika.BasicProperties(delivery_mode=2))
+        # make message persistent within the matching queues until it is received by some receiver
+        # (the matching queues have to exist and be durable and bound to the exchange)
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails
+        print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), message)
+
+
     return {
         "code": 201,
         "booking_result": {
