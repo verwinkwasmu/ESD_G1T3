b5f38caf8ad680c90f2b595223a65fc53aa695ab
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2021-04-03 17:00:39 +0800

    changes to nearest_attractions.py / book_facilities.py

--- services/nearest_attractions.py
+++ services/nearest_attractions.py
@@ -14,29 +14,40 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+GOOGLE_API_KEY = os.environ.get(
+    "GOOGLE_API_KEY") or "AIzaSyAw3Q7eJyqIazB2ucevU3NhnVmsqs5Z3bQ"
 
 @app.route("/nearest_attractions/radius=<int:radius>&type_of_attraction=<string:type_of_attraction>&lat=<float:lat>&lon=<float:lon>")
 def get_nearest_attractions(radius, type_of_attraction, lat, lon):
-    GOOGLE_API_KEY = 'AIzaSyAw3Q7eJyqIazB2ucevU3NhnVmsqs5Z3bQ'
+    
     response = requests.get(
-        f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius={radius}&type={type_of_attraction}&key={GOOGLE_API_KEY}&fields=website,rating,place_id,name,formatted_address,formatted_phone_number,photo,price_level,business_status")
+        f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius={radius}&type={type_of_attraction}&key={GOOGLE_API_KEY}")
 
     if response.status_code == 200:
         return response.json()
     else:
-        return
+        return jsonify(
+        {
+            "code": 404,
+            "message": "Attractions not found."
+        }
+    ), 404
 
 
 @app.route("/nearest_attractions/next_page/page_token=<string:page_token>")
 def get_next_page(page_token):
-    GOOGLE_API_KEY = 'AIzaSyAw3Q7eJyqIazB2ucevU3NhnVmsqs5Z3bQ'
     response = requests.get(
         f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={page_token}&key={GOOGLE_API_KEY}")
 
     if response.status_code == 200:
         return response.json()
     else:
-        return
+        return jsonify(
+        {
+            "code": 404,
+            "message": "Attractions not found."
+        }
+    ), 404
 
 
 if __name__ == "__main__":
