b17febf598d8d9087dbcc5598f3d357591ee6069
weixiangtoh <weixiangtoh982@gmail.com>
2021-04-04 12:01:37 +0800

    Update booking.py

--- services/booking/booking.py
+++ services/booking/booking.py
@@ -108,5 +108,5 @@ def set_discount(booking_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(host="0.0.0.0",port=5000, debug=False)
