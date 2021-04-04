e0a75a0b44c43dfef33cc4bddd43299b28f76a50
weixiangtoh <weixiangtoh982@gmail.com>
2021-04-04 11:55:43 +0800

    Change port for heroku

--- services/booking/booking.py
+++ services/booking/booking.py
@@ -108,4 +108,5 @@ def set_discount(booking_id):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000, debug=True)
+    port = port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0",port=5000, debug=False)
