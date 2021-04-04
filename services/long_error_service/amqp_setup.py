f366092c047474bc032071913ba904a3c98b648d
weixiangtoh <weixiangtoh982@gmail.com>
2021-04-04 16:14:16 +0800

    Add amqp cloud url

--- services/long_error_service/amqp_setup.py
+++ services/long_error_service/amqp_setup.py
@@ -4,14 +4,19 @@ from os import environ
 # These module-level variables are initialized whenever a new instance of python interpreter imports the module;
 # In each instance of python interpreter (i.e., a program run), the same module is only imported once (guaranteed by the interpreter).
 
-hostname = environ.get('rabbit_host') or 'localhost'
-port = environ.get('rabbit_port') or 5672
+# hostname = environ.get('rabbit_host') or 'localhost'
+# port = environ.get('rabbit_port') or 5672
+
+url = environ.get('CLOUDAMQP_URL', 'amqps://isqfpldg:8ydLfVqsqOIR9RkCuH32Cx6WnOrsk-6h@hornet.rmq.cloudamqp.com/isqfpldg')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+
 # connect to the broker and set up a communication channel in the connection
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host=hostname, port=port,
-        heartbeat=3600, blocked_connection_timeout=3600, # these parameters to prolong the expiration time (in seconds) of the connection
-))
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(
+#         host=hostname, port=port,
+#         heartbeat=3600, blocked_connection_timeout=3600, # these parameters to prolong the expiration time (in seconds) of the connection
+# ))
     # Note about AMQP connection: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
     # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls.
     # If see: Stream connection lost: ConnectionResetError(10054, 'An existing connection was forcibly closed by the remote host', None, 10054, None)
@@ -55,6 +60,7 @@ channel.queue_bind(exchange=exchangename, queue=queue_name, routing_key='*.short
     # bind the queue to the exchange via the key
     # any routing_key with two words and ending with '.short_error_service' will be matched
 
+
 ############   Long_Error_Service queue   #############
 #delcare Long_Error_Service queue
 queue_name = 'Long_Error_Service'
@@ -78,10 +84,13 @@ def check_setup():
     # The shared connection and channel created when the module is imported may be expired, 
     # timed out, disconnected by the broker or a client;
     # - re-establish the connection/channel is they have been closed
-    global connection, channel, hostname, port, exchangename, exchangetype
+    global connection, channel, url, params, exchangename, exchangetype
+    # url = environ.get('CLOUDAMQP_URL', 'amqps://isqfpldg:8ydLfVqsqOIR9RkCuH32Cx6WnOrsk-6h@hornet.rmq.cloudamqp.com/isqfpldg')
+    # params = pika.URLParameters(url)
 
     if not is_connection_open(connection):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
+        connection = pika.BlockingConnection(params)
+        # connection = pika.BlockingConnection(pika.ConnectionParameters(host=hostname, port=port))
     if channel.is_closed:
         channel = connection.channel()
         channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True)
