d292a98ef26c91ebf0026f18a2bbe3fab2d951aa
VerwinKwa@SMU <verwin.kwa.2019@sis.smu.edu.sg>
2021-04-11 02:25:51 +0800

    Nearest Attractions Changes

--- services/nearest_attractions/nearest_attractions.py
+++ services/nearest_attractions/nearest_attractions.py
@@ -17,9 +17,31 @@ CORS(app)
 GOOGLE_API_KEY = os.environ.get(
     "GOOGLE_API_KEY") or "AIzaSyAw3Q7eJyqIazB2ucevU3NhnVmsqs5Z3bQ"
 
+
+@app.route("/nearest_attractions/currentCoordinates")
+def get_current_coordinates():
+    response = requests.post(
+        f"https://www.googleapis.com/geolocation/v1/geolocate?key={GOOGLE_API_KEY}")
+    if response.status_code == 200:
+        result = response.json()
+        return jsonify(
+            {
+                "code": 200,
+                "data": result
+            }
+        )
+    else:
+        return jsonify(
+            {
+                "code": 400,
+                "message": "Unable to retrieve coordinates"
+            }
+        )
+
+
 @app.route("/nearest_attractions/radius=<int:radius>&type_of_attraction=<string:type_of_attraction>&lat=<float:lat>&lon=<float:lon>")
 def get_nearest_attractions(radius, type_of_attraction, lat, lon):
-    
+
     response = requests.get(
         f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius={radius}&type={type_of_attraction}&key={GOOGLE_API_KEY}")
 
@@ -27,17 +49,17 @@ def get_nearest_attractions(radius, type_of_attraction, lat, lon):
         result = response.json()
         return jsonify(
             {
-                "code" : 200,
-                "data" : result
+                "code": 200,
+                "data": result
             }
         )
     else:
         return jsonify(
-        {
-            "code": 404,
-            "message": "Attractions not found."
-        }
-    ), 404
+            {
+                "code": 404,
+                "message": "Attractions not found."
+            }
+        ), 404
 
 
 @app.route("/nearest_attractions/next_page/page_token=<string:page_token>")
@@ -49,19 +71,19 @@ def get_next_page(page_token):
         result = response.json()
         return jsonify(
             {
-                "code" : 200,
-                "data" : result
+                "code": 200,
+                "data": result
             }
         )
     else:
         return jsonify(
-        {
-            "code": 404,
-            "message": "Attractions not found."
-        }
-    ), 404
+            {
+                "code": 404,
+                "message": "Attractions not found."
+            }
+        ), 404
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5015)) or 5015
-    app.run(host="0.0.0.0",port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
